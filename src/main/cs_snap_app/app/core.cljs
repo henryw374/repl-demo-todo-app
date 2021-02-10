fcdeb2a1b110fc0919dfbb1c3f0f4f877f8c27fe
dlynnea <danimatton@gmail.com>
2021-02-09 21:28:11 -0700

    closer on bar chart, struggling with getting the word count values hooked up

--- src/main/cs_snap_app/app/core.cljs
+++ src/main/cs_snap_app/app/core.cljs
@@ -1,7 +1,7 @@
 (ns cs-snap-app.app.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [cljs.pprint :as pp] ;;for editing and debugging code
+            [cljs.pprint :as pp] ;;for editing/debugging code
             [clojure.string :as str])) 
 
 ;; --- APP STATE ---
@@ -50,31 +50,45 @@
     (swap! todos (fn [m]
                     (->> m
                         (remove g)
-                        (into (empty m))))))) ;;refactor into an mmap function 43:40 2nd vid
+                        (into (empty m))))))) ;;refactor into an mmap function 43:40 2nd
 
 ;; --- Initialize App with sample data ---
-(defn- random-point []
-  (js/Math.floor (* (js/Math.random) 10)))
-
-; (defn- random-point [total-count]
-;   total-count)
-
-(defonce chart-data
-  (let [points (map random-point (range 4))]              ;; <1>
-    (r/atom {:points points
-             :chart-max (reduce max 1 points)})))
 
 (defonce init (do
-                (add-todo "Wash dishes")
-                (add-todo "Fold laundry")
+                (add-todo "Wash the dishes")
+                (add-todo "Dry and fold laundry")
                 (add-todo "Feed cats")
-                (add-todo "Water plants")))
+                (add-todo "Water plants along windows")
+                (add-todo "Drink water!")
+                (add-todo "Breath")))
 
-(def chart-width 110)
-(def chart-height 110)
-(def bar-spacing 1)
+; (def data-set
+;   [{:id 1, :title "Wash dishes", :done true}
+;   {:id 2, :title "Fold laundry", :done true}
+;   {:id 3, :title "Feed cats", :done true}
+;   {:id 4, :title "Water plants", :done false}
+;   {:id 5, :title "Drink water", :done false}
+;   {:id 6, :title "Breath", :done false}
+;   ])
+
+; (js/console.log "!!!!!!!!!" (into {} (map (fn [[k v]] [k (f v)])) @todos))
+; (js/console.log "!????" (def filtered-seq (filter (fn [x] ...) {:id 1, :title "Wash dishes", :done true})))
+
 (def radius-lg 10)
 (def radius-sm 5)
+(def chart-width 210)
+(def chart-height 100)
+(def bar-spacing 2)
+
+; (def output (atom {}))
+
+; (defn count-word [word]
+;   (swap! output update-in [word] (fnil inc 0))) ; Update the atom with a function
+
+; (defn word-frequencies [words]
+;   (reset! output {}) ; Reset the atom to an empty map
+;   (doall (map count-word words)) ; Call count-word on each word, and do it in parallel
+;   @output) ; Return the current value of output. Blocks until output is done updating.
 
 ;; --- VIEWS ---
 
@@ -87,15 +101,13 @@
         done-count (count (filter :done items))
         active-count (- (count items) done-count)
         total-count (+ (count items))
-        done-div (/ done-count total-count)
-        done-perc (* done-div 100)
         circumf-circ (* 2 3.14 radius-sm)
         percent-val (percentage #(ratio done-count total-count)) 
-        percent-circ (/ (* percent-val circumf-circ) 100) ;;!!!figure out tech terms for stroke-dasharray
-        props-for (fn [kw]
-            {:class (when (= kw @showing) "selected")
-              :on-click #(reset! showing kw)
-              :href "#"})]
+        percent-circ (/ (* percent-val circumf-circ) 100)] ;;!!!figure out tech terms for stroke-dasharray
+        ; props-for (fn [kw]
+        ;     {:class (when (= kw @showing) "selected")
+        ;       :on-click #(reset! showing kw)
+        ;       :href "#"})]
   [:div.pie-chart
     [:h4 "Complete vs. incomplete tasks"]
     [:svg {:x 0 :y 0 :width chart-width :height chart-height :viewBox "0 0 20 20"}
@@ -104,22 +116,97 @@
                 :stroke "tomato" 
                 :stroke-width 10
                 :stroke-dasharray [percent-circ circumf-circ]
-                :transform "rotate(-90) translate(-20)"}]      
-                ]
-                [:p.small "Complete: " props-for done-count]
-                [:p "Incomplete: " props-for active-count]
-                [:p "Total: " props-for total-count]]))
+                :transform "rotate(-90) translate(-20)"}]]
+                [:p "Complete: " done-count]
+                [:p "Incomplete: " active-count]
+                [:p "Total: " total-count]]))
                       
 ;;showing active vs non values-47min
+
+; (defn count-words [s]
+;   (->> s
+;      (filter #{\space})
+;      count
+;      inc))
+
+(defn add-7
+  [num]
+  (let [answer (+ num 7)]
+    answer))
+; (println (add-7 10))
+
+; (doseq [todo data-set]
+;   (println "?" (get [1 todo] :id))
+; (defn get-eligible [x]
+;   (loop [title x]
+;     (if (empty? title)
+;       nil
+;       (let [todo (first title)]
+;         (println (vector (count (str/split (get todo :title) #"\s+"))))
+;         (recur (rest title))))))
+
+; (defn get-eligible
+;   [x]
+;   (map (fn [todo] (get todo :id))
+;        (filter (fn [todo] (>= (get todo :id) 21)) x)))
+
+(defn get-eligible-4
+  [x]
+  (into []
+        (map (fn [todo] (get todo :id))
+             (filter (fn [todo] (>= (get todo :id) 21)) x))))
+(println "vec" (vector 1 2 3))
+(defn count-words [s]
+  (count (str/split s #"\s+")))
+
+; (defn- random-point []
+;   (js/Math.floor (* (js/Math.random) 100)))
+(defn- random-point []
+  (let [items (vals @todos)]
+    (loop [title items]
+        (if (empty? title)
+          nil
+          (let [todo (first title)]
+            (println "mamamia" (count (str/split (get todo :title) #"\s+")))
+            (recur (rest title)))))))
+
+(defonce chart-data
+  (let [items (vals @todos) title-count (count (map second items))]
+    (let [points (map random-point (range title-count))] ;;this isnt updating with new to dos (defonce?)            ;; <1>
+      (r/atom {:points points
+               :chart-max (reduce max 1 points)}))))
 
 (defn bar-chart []
   [:div.bar-chart 
   [:h4 "Word count of tasks"]
-  (let [{:keys [points chart-max]} @chart-data             ;; <2>
-          bar-width (- (/ chart-width (count points))
+  (let [items (vals @todos)
+        done-count (count (filter :done items))
+        active-count (- (count items) done-count)
+        total-count (+ (count items))
+        idk-count (filter :title items)
+        map-count (map :title idk-count)
+        hmm (count (get items :title))
+        final-count (count (str/split map-count #"\s+"))]
+        (println "together ()" (map :title idk-count)) 
+        (println "[w/ :title]" (map second idk-count)) 
+        (println "fkstr" final-count)
+        (println "fkstr" (count-words map-count))
+        (loop [title items]
+            (if (empty? title)
+              nil
+              (let [todo (first title)]
+                ; (println (into [] (get todo :title)))
+                (println (count (str/split (get todo :title) #"\s+")))
+                (recur (rest title)))))
+
+  (let [{:keys [points chart-max]} @chart-data           ;; <2>
+      bar-width (- (/ chart-width (count points))
                       bar-spacing)]
-      [:svg.chart {:x 0 :y 0
-                  :width chart-width :height chart-height}
+                        (println "hm" points)
+      [:svg.chart {:x 0 
+                  :y 0
+                  :width chart-width 
+                  :height chart-height}
         (for [[i point] (map-indexed vector points)          ;; <3>
               :let [x (* i (+ bar-width bar-spacing))        ;; <4>
                     pct (- 1 (/ point chart-max))
@@ -128,10 +215,10 @@
           [:rect {:key i                                     ;; <5>
                   :x x :y y
                   :width bar-width
-                  :height bar-height}])])])
+                  :height bar-height}])]))])
 
 (defn todo-input [{:keys [title on-save on-stop]}]
-  (let [input-text (r/atom title)
+  (let [input-text (r/atom title) ;;add something similar for updating word count?
         update-text #(reset! input-text %)
         stop #(do (reset! input-text "")
                   (when on-stop (on-stop)))
@@ -195,7 +282,7 @@
   [:header.header
     [:h1 "to-do items"]
     [todo-input {:class "new-todo"
-                 :placeholder "Create new to do"
+                 :placeholder "I need to.."
                  :on-save add-todo}]])
 
 (defn footer-controls []
@@ -210,7 +297,7 @@
   (fn []
     [:div
       [:section.banner
-        [pie-chart showing]
+        [pie-chart showing] ;;prop dont need shwing there
         [bar-chart]]
       [:section.todo-app
         [todo-entry]
@@ -219,7 +306,7 @@
             [todo-list showing]
             [footer-controls]])]
         [:footer.info
-          [:p "Double-click to edit a todo"]]])))
+          [:p "Double-click to edit a todo"]]]))) ;;change into tooltip??
 
 ;; --- RENDER ---
 
